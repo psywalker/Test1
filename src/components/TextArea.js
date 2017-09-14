8c79cc1ef811be095bbc8b0cff1a7d049355b15e
psywalker <psywalker09@gmail.com>
2017-09-14 19:13:44 +0300

    last commit-2

--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -1,17 +1,38 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form'
 import { Input } from 'antd';
+
 const { TextArea } = Input;
 
-class Textarea extends React.Component {
 
-    render() {
 
-        return (
-            <div className="task__textarea">
-                <TextArea placeholder="" autosize={{ minRows: 2, maxRows: 6 }} />
-            </div>
-        );
-    }
+const renderField = ({
+    input,
+    label,
+    type,
+    meta: { touched, error, warning }
+}) => (
+
+    <div className="task__textarea">
+        <TextArea {...input} type={type} placeholder={label}  autosize={{ minRows: 2, maxRows: 6 }} />
+    </div>
+)
+const Textarea = (props) => {
+    const { handleSubmit} = props
+    return (
+        <form onSubmit={handleSubmit}>
+            <Field name="username"  component={renderField} label="Username"   />
+        <Field
+    name="firstName"
+    component="input"
+    type="textarea"
+    placeholder="First Name"
+        />
+        </form>
+
+    )
 }
+export default reduxForm({
+    form: 'syncValidation',  // a unique identifier for this form
 
-export default Textarea;
+})(Textarea)
--- src/index.js
+++ src/index.js
@@ -2,7 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux'
+import { createStore, combineReducers } from "redux";
 import registerServiceWorker from './registerServiceWorker';
+import { reducer as form } from "redux-form";
+const todos = (state = [], action) => {
 
-ReactDOM.render(<App />, document.getElementById('root'));
+}
+const reducer = combineReducers({ form });
+const store = createStore(reducer)
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
 registerServiceWorker();
